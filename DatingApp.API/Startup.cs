781eb8f15e12d1508fa8b1b8d56e00c5f8e01026
alexanderkounadis <alexanderkounadis@gmail.com>
2019-08-15 16:03:41 +0300

    Routes, guards and cors support to the API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -64,7 +64,7 @@ namespace DatingApp.API {
             }
 
             // app.UseHttpsRedirection();
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc ();
         }
